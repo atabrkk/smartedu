962c78da85186fd2f8caace5987cbe1c00ba367a
Ataberk Özyılmaz <ataberk279@gmail.com>
2023-07-08 18:43:48 +0300

    model yazıldı ve admin sayfası ayarlandı

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # My Apps
     'pages',
+    'courses'
 ]
 
 MIDDLEWARE = [
@@ -115,6 +116,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')  # static klasörünü belirttik
 ]
+
+# MEDIA FILES
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=200, unique=True)  # verbose_name: admin alanında gözükürken nasıl gözükeceği. help_text: admin kısmında kullancığa açıklayıcı bilgi.
+    description = models.TextField(blank=True)  # blank:True-> son kullanıcı açısından boş olabilir. null:True-> dev açısından boş olabilir.
+    image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/default_course_image.jpg")
+    date = models.DateTimeField(auto_now=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
